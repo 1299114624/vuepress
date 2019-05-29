e6f6bea8e2592bed8050c9f087a17963f9983e9b
1299114624 <1299114624@qq.com>
2019-05-29 20:11:50 +0800

    save

---
+++ docs/.vuepress/components/Democode.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="code">
+        <div class="code--title">
+            <h2>{{title}}</h2>
+            <small>{{description}}</small>
+        </div>
+        <div class="code--demo">
+            <div class="code-content">
+                <slot></slot>
+            </div>
+        </div>
+        <div v-if="isShow" class="code--segmennt">
+            <slot name="codeText"></slot>
+        </div>
+        <div class="code--button" v-if="$slots.codeText" @click="handleToggleShow">{{codeTextBtn}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['title', 'description'],
+    data(){
+        return{
+            isShow: false,
+            codeTextBtn: '显示代码'
+        }
+    },
+    methods:{
+        handleToggleShow(){
+            this.isShow = !this.isShow;
+            this.codeTextBtn = this.isShow ? '隐藏代码' : '显示代码'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
